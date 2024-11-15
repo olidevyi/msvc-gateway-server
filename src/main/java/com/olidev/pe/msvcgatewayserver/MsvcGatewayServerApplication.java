39d4447fc3dcb154ad5e4d46617eff832ee255c9
olidevyi <jean.yantas@gmail.com>
2024-11-15 09:19:27 -0500

    configuracion basica ok

---
+++ src/main/java/com/olidev/pe/msvcgatewayserver/MsvcGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.olidev.pe.msvcgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcGatewayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olidev/pe/msvcgatewayserver/MsvcGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olidev.pe.msvcgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
